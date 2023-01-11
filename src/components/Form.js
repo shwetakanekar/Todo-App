5aa9440f6cbd2e78c57e87e40e1a7fa479b6d75c
Shweta Kanekar <kanekarshweta10@gmail.com>
2023-01-11 18:03:33 +0530

    Added items to Todo list

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,29 @@
 import styles from '../styles.module.css';
 
-const Form = () => {
+const Form = ({ todo, setTodo, todoList, setTodoList }) => {
+  const handleInputChange = (event) => {
+    setTodo(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setTodoList([...todoList, todo]);
+    setTodo('');
+  };
+
   return (
     <div className={styles.todoForm}>
-      <form>
-        <input className={styles.todoInput} placeholder="Todo item" />
-        <button className={styles.todoButton}>Add</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          className={styles.todoInput}
+          value={todo}
+          placeholder="Todo item"
+          onChange={handleInputChange}
+          required
+        />
+        <button type="submit" className={styles.todoButton}>
+          Add
+        </button>
       </form>
     </div>
   );
